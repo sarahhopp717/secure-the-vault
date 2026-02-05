c531ab5492d85caa2e864ee4f8381a3a27d01272
Sarah Hopp <sarahhopp717@gmail.com>
2026-02-04 18:57:37 -0800

    Finished homework

---
+++ index.js
@@ -0,0 +1,19 @@
+let code1 = 7 + 3;
+
+console.log(code1);
+
+let code2 = (100 - 20) / 2;
+
+console.log(code2);
+
+let code3 = 100 - 61;
+
+console.log(code3);
+
+let codeA = code1 - code2 - code3;
+
+console.log(codeA);
+
+let codeB = `${code1} - ${code2} - ${code3}`;
+
+console.log(codeB);
